dfc68babe1e8e2529726776bd9ec89113fecf3e4
Anurag <138777568+anuragastech@users.noreply.github.com>
2023-07-30 21:33:54 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,7 +51,7 @@
 
     $(function () {
         element.typed({
-            strings: ["Web Designer.", "Graphic Designer."],
+            strings: ["Web Designer.", "web Developer."],
             typeSpeed: 100,
             loop: true,
             autoplay: true,
